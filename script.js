1a04bb60bd31b0c77528455e4d48259a44675dd4
emilygarvey <41160828+emilygarvey@users.noreply.github.com>
2018-07-18 17:00:23 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,11 @@
+$(".aztec button").click(function() {
+    $(".aztec img").fadeOut();
+});
+
+$(".buryButton").click(function() {
+    $(".bury img").slideUp();
+});
+
+$(".poof button").click(function() {
+    $(".poof img").show();
+});
